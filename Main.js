3f57b62252042560985300c7e9d62b5782696999
Trubetskoy <42418728+Trubetskoy@users.noreply.github.com>
2018-11-01 10:00:20 +0200

    Fixed NaN, made 3 functions instead one, fixed if/else

--- Main.js
+++ Main.js
@@ -1,4 +1,4 @@
-function Loops(array, b) {
+function Max(array, b) {
     let result;
     if (b === 'max') {
         result = array[0];
@@ -8,8 +8,13 @@ function Loops(array, b) {
                     result = array[i]
             }
         }
+    }
+    return result;
+}
 
-    } else if (b === 'min') {
+function Min(array, b) {
+    let result;
+    if (b === 'min') {
         result = array[0];
         for (let i = 0; i < array.length; i++) {
             if (array[i] !== -Infinity) {
@@ -17,12 +22,17 @@ function Loops(array, b) {
                     result = array[i];
             }
         }
+    }
+    return result;
+}
 
-    } else if (b === 'summ') {
+
+function Summ(array, b) {
+    let result;
+    if (b === 'summ') {
         result = 0;
         for (let i = 0; i < array.length; i++) {
-            if (array[i] === -Infinity || array[i] === Infinity || isNaN(array[i])) {
-            } else if (typeof (array[i]) === 'number') {
+            if (array[i] !== null && array[i] !== undefined && isFinite(array[i])) {
                 result += (array[i])
             }
         }
@@ -30,11 +40,11 @@ function Loops(array, b) {
     return result;
 }
 
-console.log('Maximum', Loops([3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2 - 3 - 1], 'max'));
-console.log('Minimum', Loops([3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2 - 3 - 1], 'min'));
-console.log('Summ', Loops([3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2 - 3 - 1], 'summ'));
+console.log('Maximum', Max([3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2 - 3 - 1], 'max'));
+console.log('Minimum', Min([3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2 - 3 - 1], 'min'));
+console.log('Summ', Summ([3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2 - 3 - 1], 'summ'));
 
-console.log('Maximum', Loops([1, undefined, NaN, Infinity, -Infinity, 'asluirgfv', 3, 5, -3], 'max'));
-console.log('Minimum', Loops([1, undefined, NaN, Infinity, -Infinity, 'asluirgfv', 3, 5, -3], 'min'));
-console.log('Summ', Loops([1, undefined, NaN, Infinity, -Infinity, 'asluirgfv', 3, 5, -3], 'summ'));
+console.log('Maximum', Max([1, undefined, NaN, Infinity, -Infinity, 'asluirgfv', 3, 5, -3], 'max'));
+console.log('Minimum', Min([1, undefined, NaN, Infinity, -Infinity, 'asluirgfv', 3, 5, -3], 'min'));
+console.log('Summ', Summ([1, undefined, NaN, Infinity, -Infinity, 'asluirgfv', 3, 5, -3], 'summ'));
 
